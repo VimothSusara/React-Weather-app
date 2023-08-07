dac3e89ae7e1ed07ad41b20b1a8a4106fc0cfba9
VimothSusara <116062952+VimothSusara@users.noreply.github.com>
2023-08-07 18:49:48 +0530

    weatherWidget,search bar and css partially com.

---
+++ weather-app/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "9f9d6a14ffmsh9a1cfaf06b5192dp143a8fjsn6139d1d905fb",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "8dc5abcf9e09d578eecf5bc18c0550b9";
